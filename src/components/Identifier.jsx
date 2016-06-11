1ea041edd973d2d63162a58b8e06abbdc144d07c
Ryan Shaw <ryanshaw@unc.edu>
2016-06-11 12:41:34 -0400

    Merge Deletable and RoundedRectangle.

--- src/components/Identifier.jsx
+++ src/components/Identifier.jsx
@@ -1,22 +1,15 @@
 const React = require('react') // eslint-disable-line no-unused-vars
     , RoundedRectangle = require('./RoundedRectangle')
-    , DeletableRoundedRectangle = require('./DeletableRoundedRectangle')
 
 const Identifier = ({id, label = id, onClick, onClickDelete}) => (
   id === undefined
     ? <span className="blank-node"></span>
-    : onClickDelete
-        ? <DeletableRoundedRectangle
-            text={label}
-            classes={`bg-blue ${onClick ? '' : 'muted'}`}
-            onClick={onClick}
-            onClickDelete={onClickDelete}
-          />
-        : <RoundedRectangle
-            text={label}
-            classes={`bg-blue ${onClick ? '' : 'muted'}`}
-            onClick={() => window.open(id, '_blank')}
-        />
+    : <RoundedRectangle
+        text={label}
+        classes="bg-blue"
+        onClick={onClick}
+        onClickDelete={onClickDelete}
+      />
 )
 
 Identifier.propTypes = {
--- src/components/RoundedRectangle.jsx
+++ src/components/RoundedRectangle.jsx
@@ -1,15 +1,34 @@
 const React = require('react') // eslint-disable-line no-unused-vars
+    , DeleteButton = require('./DeleteButton')
 
-const RoundedRectangle = ({text, classes = '', onClick}) => (
+const RoundedRectangle = (
+  {text, classes = '', onClick, onClickDelete}) => (
   <span
-    className={
-      `relative inline-block py1 px2 white bold cursor-pointer
+    className={`relative inline-block py1 pl2 white bold
  ${classes}
-`}
-    style={{borderRadius: '1.5em', minHeight: '1.5em'}}
+ ${onClick ? 'cursor-pointer' : 'muted'}
+ ${onClickDelete ? 'pl2' : 'px2'}`}
+    style={
+      { borderRadius: '1.5em'
+      , minHeight: '1.5em'
+      , paddingRight: onClickDelete ? '2.5em' : null
+      }
+    }
     onClick={onClick}
   >
     <span className="align-middle">{text}</span>
+    <DeleteButton
+      color="#ffffff"
+      classes={`absolute top-0 right-0
+ ${onClickDelete ? 'cursor-pointer' : 'hidden'}`}
+      onClick={onClickDelete
+        ? e => {
+            e.stopPropagation()
+            onClickDelete()
+          }
+        : null
+      }
+    />
   </span>
 )
 
@@ -17,6 +36,7 @@ RoundedRectangle.propTypes = {
   text: React.PropTypes.string.isRequired,
   classes: React.PropTypes.string,
   onClick: React.PropTypes.func,
+  onClickDelete: React.PropTypes.func
 }
 
 module.exports = RoundedRectangle
