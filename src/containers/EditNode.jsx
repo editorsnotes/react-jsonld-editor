1dd0e563ce3ed2ecad30b2a65f9aa3dc00e5d557
Ryan Shaw <ryanshaw@unc.edu>
2016-10-20 19:17:09 -0400

    EditNode needs dispatch props.

--- src/containers/EditNode.jsx
+++ src/containers/EditNode.jsx
@@ -24,7 +24,7 @@ const React = require('react') // eslint-disable-line no-unused-vars
     , Type = require('./Type')
     , Property = require('./Property')
     , AddProperty = require('./AddProperty')
-    , {keyFromPath, labelOrID, keepOnlyStateProps} = require('../utils')
+    , {keyFromPath, labelOrID} = require('../utils')
 
 const mapStateToProps = state => (
   { top: getNode(state)
@@ -145,4 +145,4 @@ const EditNode = (
 }
 
 module.exports = connect(
-  mapStateToProps, mapDispatchToProps, keepOnlyStateProps)(withRebass(EditNode))
+  mapStateToProps, mapDispatchToProps)(withRebass(EditNode))
